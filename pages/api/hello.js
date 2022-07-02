90f327112a85f86ebd7856ca86b16ba32a07b336
elprosystem <contato@elprosystem.com.br>
2022-07-02 14:49:01 -0300

    type module

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'elprosystem', type: 'pai-v1' })
+  res.status(200).json({ name: 'elprosystem', type: 'api-v1' })
 }
